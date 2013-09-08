ff06ca61ed64e6e15b55228f8a190c79797b31a5
Liam Bailey <info@webbyscots.com>
2013-09-08 23:55:20 +0100

    Tidy up main php file and renamed + removed unnecessary admin css file

--- prettyWPGallery.php
+++ prettyWPGallery.php
@@ -12,35 +12,36 @@
   License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 $ppGall = new prettyGallery();
+
 class prettyGallery {
 
     function __construct() {
         define('WSWPPGPATH', trailingslashit(dirname(__FILE__)));
-        define('WSWPDEBUG_MODE',false);
+        define('WSWPDEBUG_MODE', false);
         define('WSWPPGURL', trailingslashit(plugin_dir_url(__FILE__)));
-        add_action('wp_enqueue_scripts',array(&$this,'wp_enqueue_scripts'));
+        add_action('wp_enqueue_scripts', array(&$this, 'wp_enqueue_scripts'));
         remove_shortcode('gallery', 'gallery_shortcode');
-        add_shortcode('gallery', array(&$this, 'gallery_shortcode_delux'));
-    }
-    
-    function wp_enqueue_scripts() {
-        wp_enqueue_script( 'prettyphoto', WSWPPGURL . 'js/jquery.prettyPhoto.min.js', array( 'jquery' ), '3.1.4',true );
-        wp_enqueue_script( 'prettygallery', WSWPPGURL . 'js/prettyGallery.js',array('prettyphoto'),'1.0.0',true);
-        wp_enqueue_style( 'prettyphoto',  WSWPPGURL . 'css/prettyPhoto.css', false, '3.1.4', 'screen' );
+        add_shortcode('gallery', array(&$this, 'gallery_shortcode_prettyPhoto'));
     }
 
+    function wp_enqueue_scripts() {
+        wp_enqueue_script('prettyphoto', WSWPPGURL . 'js/jquery.prettyPhoto.min.js', array('jquery'), '3.1.4', true);
+        wp_enqueue_script('prettygallery', WSWPPGURL . 'js/prettyGallery.js', array('prettyphoto'), '1.0.0', true);
+        wp_enqueue_style('prettyphoto', WSWPPGURL . 'css/prettyPhoto.css', false, '3.1.4', 'screen');
+    }
 
     /**
-     * The Gallery shortcode Deluxe.
+     * The Gallery shortcode replacement.
      *
      * Completely ripped off from the WP Gallery Shortcode function. 
      * Thank god it is open source - but adds the rel attribute needed
-     * to make our galleries play with the prettyPhoto media plugin.
+     * to fire prettyPhoto. It also hides all but the first image, leaving
+     * links to form the gallery.
      *
      * @param array $attr Attributes of the shortcode.
      * @return string HTML content to display gallery.
      */
-    function gallery_shortcode_delux($attr) {
+    function gallery_shortcode_prettyPhoto($attr) {
         $post = get_post();
 
         static $instance = 0;
@@ -52,7 +53,7 @@ class prettyGallery {
                 $attr['orderby'] = 'post__in';
             $attr['include'] = $attr['ids'];
         }
-        
+
         $is_active = (isset($attr['link']) && 'file' == $attr['link']);
 
         // Allow plugins/themes to override the default gallery template.
@@ -152,42 +153,33 @@ class prettyGallery {
 
         $i = 0;
         foreach ($attachments as $id => $attachment) {
-            if (isset($attr['link']) && 'file' == $attr['link']) {
-                /*$image =
-                $link = ($i == 0) ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, 'full', false, false,'a');
-                $link = preg_match( '/rel="/', $link ) ? str_replace('rel="', 'rel="' . $rel . '"', $link ) : str_replace( '<a ', '<a rel="' . $rel .'"', $link );
-                if ($i > 0) {
-                  $link = preg_match('/style="/',$link) ? str_replace('/style="/','/style="display:none"/',$link) : str_replace("<a ","<a style='display:none' ");
-                } */
-                $bigurl = wp_get_attachment_image_src($id,'full',false);
+            if ($is_active) {
+                $bigurl = wp_get_attachment_image_src($id, 'full', false);
                 $bigurl = $bigurl[0];
-                $anchor = ($i==0) ? wp_get_attachment_image($id,$size,false) : "";
+                $anchor = ($i == 0) ? wp_get_attachment_image($id, $size, false) : "";
                 $link = "<a href='{$bigurl}' rel='{$rel}'>{$anchor}</a>";
-
             } else {
                 $link = wp_get_attachment_link($id, $size, true, false);
             }
 
             $output .= ($is_active && $i == 0 || !$is_active) ? "<{$itemtag} class='gallery-item'>" : "";
             $output .= ($is_active && $i == 0 || !$is_active) ? "<{$icontag} class='gallery-icon'>" : "";
-		$output .= $link;
-		$output .= ($is_active && $i == count($attachments)-1 || !$is_active) ? "</{$icontag}>" : "";
-            
-                if ($captiontag && trim($attachment->post_excerpt) && ($is_active && $i == 0 || !$is_active)) {
-                    $output .= "
-				<{$captiontag} class='wp-caption-text gallery-caption'>
-				" . wptexturize($attachment->post_excerpt) . "
-				</{$captiontag}>";
-                }
-            
-             $output .= ($is_active && $i == count($attachments)-1 || !$is_active) ? "</{$itemtag}>" : "";
-            if ($columns > 0 && ++$i % $columns == 0  && ($is_active && $i == 0 || !$is_active))
+            $output .= $link;
+            $output .= ($is_active && $i == count($attachments) - 1 || !$is_active) ? "</{$icontag}>" : "";
+
+            if ($captiontag && trim($attachment->post_excerpt) && ($is_active && $i == 0 || !$is_active)) {
+                $output .= "<{$captiontag} class='wp-caption-text gallery-caption'>";
+                $output .= wptexturize($attachment->post_excerpt);
+		$output .= "</{$captiontag}>";
+            }
+
+            $output .= ($is_active && $i == count($attachments) - 1 || !$is_active) ? "</{$itemtag}>" : "";
+            if ($columns > 0 && ++$i % $columns == 0 && ($is_active && $i == 0 || !$is_active))
                 $output .= '<br style="clear: both" />';
         }
 
-        $output .= "
-			<br style='clear: both;' />
-		</div>\n";
+        $output .= "<br style='clear: both;' />";
+	$output .= "</div>\n";
 
         return $output;
     }
